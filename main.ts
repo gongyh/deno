fdba32429f348d79d8167224a7600f67309d5ec7
Yanhai Gong <gongyh211@163.com>
2023-03-16 21:34:36 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
+const { instance, module } = await WebAssembly.instantiateStreaming(
+  fetch("https://wpt.live/wasm/incrementer.wasm"),
+);
+
+const increment = instance.exports.increment as (input: number) => number;
+console.log(increment(41));
+
 serve((_req) => {
-  return new Response("Hello World!", {
+  return new Response("Hello World! " + increment(41), {
     headers: { "content-type": "text/plain" },
   });
 });
